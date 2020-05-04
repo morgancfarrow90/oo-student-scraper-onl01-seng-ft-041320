aa52070265b74c98b969a272fb6d3a06ec2bebdb
Morgan Farrow <morgancfarrow@gmail.com>
2020-05-04 19:47:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,9 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    student_hash.each do |k,v|
+      instance_variable_set("@#{k}", v) unless v.nil?
+    end
   end
 
   def self.create_from_collection(students_array)
