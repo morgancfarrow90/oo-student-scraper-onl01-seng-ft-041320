5ae7193387aa72c7855d390a13fee0885474286b
Morgan Farrow <morgancfarrow@gmail.com>
2020-05-04 19:10:12 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -15,7 +15,7 @@ class Scraper
     student.profile_url = doc.search('a.href').text
     
     student.collect do |student| 
-      students = [:name = student.name, :location = student.location, :profile_url = student.profile_url]
+      students = [:name => student.name, :location => student.location, :profile_url => student.profile_url]
   
     end
   end
