6e4c8edcecd29be3fa1717575662d893e8d43331
Morgan Farrow <morgancfarrow@gmail.com>
2020-05-04 19:14:29 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,12 +6,20 @@ class Scraper
   attr_accessor :name, :location, :profile_url
 
   def self.scrape_index_page(index_url)
-    students = {}
-    doc = Nokogiri::HTTP(open(https://learn-co-curriculum.github.io/student-scraper-test-page/index.html))
-    
-    
-  
+    def self.scrape_index_page(index_url)
+    students = []
+    html = open(index_url)
+    index = Nokogiri::HTML(html)
+    index.css("div.student-card").each do |student|
+      student_details = {}
+      student_details[:name] = student.css("h4.student-name").text
+      student_details[:location] = student.css("p.student-location").text
+      profile_path = student.css("a").attribute("href").value
+      student_details[:profile_url] = './fixtures/student-site/' + profile_path
+      students << student_details
     end
+    students
+  end
   end
 
   def self.scrape_profile_page(profile_url)
