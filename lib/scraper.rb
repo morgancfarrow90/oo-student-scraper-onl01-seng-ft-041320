5c307823e80070f47e8794ced2b082d8c0c07cce
Morgan Farrow <morgancfarrow@gmail.com>
2020-05-04 19:13:41 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,13 +9,7 @@ class Scraper
     students = {}
     doc = Nokogiri::HTTP(open(https://learn-co-curriculum.github.io/student-scraper-test-page/index.html))
     
-    student = self.new
-    student.name = doc.search('h4.student-name').text 
-    student.location = doc.search('p.student-location').text
-    student.profile_url = doc.search('a.href').text
     
-    student.collect do |student| 
-      students = [:name => student.name, :location => student.location, :profile_url => student.profile_url]
   
     end
   end
